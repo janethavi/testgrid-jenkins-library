28c40ad0115ea8cb09e71cd48c6123e6e0a4533a
janethavi <janethavishka@gmail.com>
2022-06-07 19:54:44 +0530

    improve exit

--- scripts/is/deploy.sh
+++ scripts/is/deploy.sh
@@ -76,8 +76,10 @@ do
         log_error "CloudFormation file errors identified!"
         aws cloudformation describe-stack-events --stack-name ${stackName} --region ${region} |  jq -r '.StackEvents[] | select(.ResourceStatus=="CREATE_FAILED")'
         #bash ${currentScript}/../post-actions.sh ${deploymentName}
+        echo "--------"
         set -e
         exit 1
+        echo "--------"
     fi
 
     # Add a proper comment  why this if confition was used
